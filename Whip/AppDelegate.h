e1ca1baafb7b5a8530d80ef1949f4f69316b31f6
Allen White <awhite23@rocketmail.com>
2015-10-10 18:44:20 -0400

    first push

---
+++ Whip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Whip
+//
+//  Created by Allen White on 10/8/15.
+//  Copyright © 2015 Worldwide International. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Whip/GamePlayViewController.h
@@ -0,0 +1,14 @@
+//
+//  GamePlayViewController.h
+//  Whip
+//
+//  Created by Allen White on 10/8/15.
+//  Copyright © 2015 Worldwide International. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GamePlayViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *feedbackLabel;
+
+@end
